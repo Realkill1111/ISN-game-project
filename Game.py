7ccbcd14085c861bf3f99ff1e6c9d6e55c79455d
Realkill1111 <60888345+Realkill1111@users.noreply.github.com>
2020-04-12 11:41:00 +0200

    Add files via upload

--- Game.py
+++ Game.py
@@ -4,6 +4,65 @@ import os
 import tkinter
 from threading import Timer
 
+
+def bouton(fenetre,x, y, w, h, backgroundcolor, texte, textcolor, fnt):
+    box = ( (x,y) , (w,h))
+    pygame.draw.rect(fenetre, backgroundcolor, box)
+    txt = fnt.render(texte, True, textcolor)
+    txtpos = txt.get_rect()
+    txtpos.x = x+int(w/2) - int(txtpos.width/2)
+    txtpos.y = y+int(h/2) - int(txtpos.height/2)
+    fenetre.blit(txt, txtpos)
+    return ( Rect(x, y, w, h) )
+
+def centering(w,h,img):
+    ww=img.get_rect().size
+    return (abs(w-ww[0])/2) , (abs(h-ww[1])/2)
+
+def liremap(ligne):
+    m=[]
+    s = ligne[ligne.find('=')+1 : len(ligne)].split(',')
+    for i in s:
+        n = int(i)
+        m.append(n)
+    return m
+
+def dedansPoint(pos, rect) :
+    x1 = rect.x
+    y1 = rect.y
+    x2 = rect.x+rect.w
+    y2 = rect.y+rect.h
+    if x1 <= pos[0] <= x2 and y1 <= pos[1] <= y2:
+        return True
+    return False
+
+def dedans(r1, r2):
+    r1w = r1.x+r1.w
+    r1h = r1.y+r1.h
+    r2w = r2.x+r2.w
+    r2h = r2.y+r2.h
+    if r1.x <= r2.x <= r1w and r1.y <= r2.y <= r1h and r1.x <= r2w <= r1w and r1.y <= r2h <= r1h:
+        return True
+    return False
+
+def touche(r1,r2):
+    r1w = r1.x+r1.w
+    r1h = r1.y+r1.h
+    r2w = r2.x+r2.w
+    r2h = r2.y+r2.h
+    print(r1.x , "<=" , r2.x ,'<=', r1w, 'and' ,r1.x , '<=' , r2w , '<=' , r1w , 'and' , r1h ,'>=' , r2.y)
+    if r1.x <= r2.x <= r1w and r1.x <= r2w <= r1w and r1h >= r2.y:
+        return True
+    return False
+
+def coord(pos,orig):
+    #posx,posy = pygame.mouse.get_pos()
+    fnt = pygame.font.Font("freesansbold.ttf", 24)  # #AnonymousPro-Regular.ttf
+    text = fnt.render(f"{pos[0]}, {pos[1]}",1,(255,255,255))
+    r = (text.get_rect().x+xdisp, text.get_rect().y+ydisp, text.get_rect().w+20, text.get_rect().h+20)
+    pygame.draw.rect(fenetre, noir, r)
+    fenetre.blit(text, (xdisp+orig[0],ydisp+orig[1]))
+
 # Detection taille écran
 root = tkinter.Tk()
 scrwidth = root.winfo_screenwidth()
@@ -13,33 +72,13 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
 pygame.init()
 
 # Ouverture de la fenêtre Pygame
-fenetre = pygame.display.set_mode((scrwidth, scrheight),pygame.NOFRAME)
-
-if scrwidth >= 1920 and scrheight >= 1080:
-    leng = 1
-    perso_x = 960
-    perso_y = 840
-    WIDTH = 48
-    h = 48
-    w = 52
-elif scrwidth <= 1920 and scrheight <= 1080:
-    leng = 0.71
-    perso_x = 680
-    perso_y = 595
-    WIDTH = 34
-    h = 34
-    w = 37
+fenetre = pygame.display.set_mode((scrwidth, scrheight))
 
 path = os.path.dirname(os.path.abspath(__file__))
 combined_size = f"{scrwidth}x{scrheight}"
 sprites_dir = f"{path}/sprites"
 menu = True
 continuer = False
-
-def centering(img):
-    global scrwidth,scrheight
-    ww=img.get_rect().size
-    return (abs(scrwidth-ww[0])/2) , (abs(scrheight-ww[1])/2)
 
 xdisp,ydisp = 0,0
 
@@ -49,26 +88,8 @@ rouge = 255,   0,   0
 jaune = 255, 220,   0
 
 police = pygame.font.Font('freesansbold.ttf',40)
-
-def bouton(x, y, w, h, backgroundcolor, texte, textcolor, fnt):
-    global fenetre
-    box = ( (x,y) , (w,h))
-    pygame.draw.rect(fenetre, backgroundcolor, box)
-    txt = fnt.render(texte, True, textcolor)
-    txtpos = txt.get_rect()
-    txtpos.x = x+int(w/2) - txtpos.width/2
-    txtpos.y = y+int(h/2) - txtpos.height/2
-    fenetre.blit(txt, txtpos)
-    return ( Rect(x, y, w, h) )
-
-jouer   = bouton((scrwidth-440)/2, (scrheight-200)/2, 440, 75, rouge, "JOUER"  , jaune, police)
-quitter = bouton((scrwidth-440)/2, (scrheight+200)/2, 440, 75, rouge, "QUITTER", jaune, police)
-
-
-ecran = []
-salle = []
-pn = []
-ps = []
+jouer   = bouton(fenetre, (scrwidth-440)/2, (scrheight-200)/2, 440, 75, rouge, "JOUER"  , jaune, police)
+quitter = bouton(fenetre,(scrwidth-440)/2, (scrheight+200)/2, 440, 75, rouge, "QUITTER", jaune, police)
 
 #Rafraîchissement de l'écran
 pygame.display.flip()
@@ -77,31 +98,17 @@ pygame.mixer.music.load(f"{path}/Musique/twimenu.wav")
 posx,posy = pygame.mouse.get_pos()
 pygame.mixer.music.play()
 
-def dedans(pos, rect) :
-    x1 = rect.x
-    y1 = rect.y
-    x2 = rect.x+rect.w
-    y2 = rect.y+rect.h
-    if x1 <= pos[0] <= x2 and y1 <= pos[1] <= y2:
-        return True
-    return False
-
+menu = False
 while menu:
     
     for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE) or (event.type == MOUSEBUTTONUP and dedans(pygame.mouse.get_pos(), quitter) ):
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or (event.type == pygame.MOUSEBUTTONUP and dedansPoint(pygame.mouse.get_pos(), quitter) ):
             menu = False
             continuer = False
             
-        if event.type == MOUSEMOTION:
-            posx,posy = pygame.mouse.get_pos()
-            fnt = pygame.font.Font("freesansbold.ttf", 24)
-            text = fnt.render(f"x,y: ({posx}, {posy})",1,(255,255,255))
-            r = (text.get_rect().x+xdisp, text.get_rect().y+ydisp, text.get_rect().w+20, text.get_rect().h)
-            pygame.draw.rect(fenetre, noir, r)
-            fenetre.blit(text, (xdisp,ydisp))
+        #if event.type == pygame.MOUSEMOTION:
 
-        if event.type == MOUSEBUTTONUP and dedans(pygame.mouse.get_pos(), jouer):
+        if event.type == pygame.MOUSEBUTTONUP and dedansPoint(pygame.mouse.get_pos(), jouer):
             pygame.init()
             fenetre.fill((0,0,0))
             menu = False
@@ -109,23 +116,7 @@ while menu:
             pygame.mixer.music.stop()
     
             # Chargement et collage du fond + perso
-            frame = 0
-            """
-            arena = pygame.image.load(f"{sprites_dir}/background.png").convert_alpha()
-            arena = pygame.transform.scale(arena, (scrwidth,scrheight))
-            layer = pygame.image.load(f"{sprites_dir}/layerblit.png").convert_alpha()
-            layer = pygame.transform.scale(layer, (scrwidth,scrheight))
-            fond = pygame.image.load(f"{sprites_dir}/back.png").convert_alpha()
-            fond = pygame.transform.scale(fond, (scrwidth,scrheight))
-            perso_up = pygame.image.load(f"{sprites_dir}/move/perso_up.png").convert_alpha()
-
-            fenetre.blit(arena, (xdisp,ydisp))
-            fenetre.blit(perso_up, (perso_x,perso_y))
-
-            #perso_run_up = pygame.image.load(f"{sprites_dir}/move/perso_run_up.png").convert_alpha()
-            """
-            frame_rect = pygame.Rect(frame * WIDTH, 0, h, w)
-
+            
             clock = pygame.time.Clock()
             
 
@@ -138,53 +129,42 @@ while menu:
 
         pygame.display.flip()
 
+frame = 0
 niveau = 1
-
 arena = 0
 echelle=0
 layer =0
 fond = 0
 perso_up =0
 perso_run_up = 0
+speed = 10
+trigger = 0
+
+ecran = []
+salle = Rect(0,0,0,0)
+pn = Rect(0,0,0,0)
+ps = Rect(0,0,0,0)
 
 def charge(niveau):
     global ecran, salle, pn, ps
-    global echelle, arena, layer,fond, perso_up, perso_run_up, fenetre
+    global echelle, arena, layer, fond, perso_up, perso_run_up
     d = f"{sprites_dir}/level{niveau}"
     with open(f"{d}/map.txt", 'r') as map:
         for ligne in map:
             ligne = ligne.rstrip('\n')
             if 'ecran' in ligne: 
-                ecran = ligne[ligne.find('=')+1 : len(ligne)].split(',')
-                i=0
-                for s in range(0, len(ecran)):
-                    n = int(ecran[i])
-                    ecran[i] = n
-                    i=i+1
-                #ecran = list(map(int, ecran)) 
+                ecran = liremap(ligne)
             if 'salle' in ligne:
-                salle = ligne[ligne.find('=')+1 : len(ligne)].split(',')
-                i=0
-                for s in range(0, len(salle)):
-                    n = int(salle[i])
-                    salle[i] = n
-                    i=i+1
+                msalle=liremap(ligne)
+                salle = Rect(msalle[0],msalle[1],msalle[2]-msalle[0],msalle[3]-msalle[1])
             if 'porte_nord' in ligne:
-                pn = ligne[ligne.find('=')+1 : len(ligne)].split(',')
-                i=0
-                for s in range(0, len(pn)):
-                    n = int(pn[i])
-                    pn[i] = n
-                    i=i+1
+                mpn = liremap(ligne)
+                pn = Rect(mpn[0],mpn[1],mpn[2]-mpn[0],mpn[3]-mpn[1])
             if 'porte_sud' in ligne:
-                ps = ligne[ligne.find('=')+1 : len(ligne)].split(',')
-                i=0
-                for s in range(0, len(ps)):
-                    n = int(ps[i])
-                    ps[i] = n
-                    i=i+1
+                mps = liremap(ligne)
+                ps = Rect(mps[0],mps[1],mps[2]-mps[0],mps[3]-mps[1])
 
-    echelle = (ecran[0] / scrwidth , ecran[1] / scrheight)
+    echelle = (scrwidth/ecran[0] , scrheight/ecran[1])
     arena = pygame.image.load(f"{d}/background.png").convert_alpha()
     arena = pygame.transform.scale(arena, (scrwidth,scrheight))
     layer = pygame.image.load(f"{d}/layerblit.png").convert_alpha()
@@ -192,100 +172,73 @@ def charge(niveau):
     fond = pygame.image.load(f"{d}/back.png").convert_alpha()
     fond = pygame.transform.scale(fond, (scrwidth,scrheight))
     perso_up = pygame.image.load(f"{d}/move/perso_up.png").convert_alpha()
-    #perso_up = pygame.transform.scale(perso_up, (echelle[0],echelle[1]))
     perso_run_up = pygame.image.load(f"{d}/move/perso_run_up.png").convert_alpha()
-    #perso_run_up = pygame.transform.scale(perso_run_up, (echelle[0],echelle[1]))
-    fenetre.blit(arena, (xdisp,ydisp))
-    fenetre.blit(perso_up, (perso_x,perso_y))
 
-            
+charge(niveau)
+
+pygame.time.Clock().tick(60)
+pygame.key.set_repeat(1,10)
+
+perso = Rect(salle.x+salle.w/2,salle.y+salle.h/2,48,52)
+
+continuer = True
 while continuer:
 
-    charge(niveau)
-     
+    fenetre.blit(arena, (xdisp,ydisp))
+    fenetre.blit(layer, (xdisp,ydisp))
+    #fenetre.blit(perso_up, ( int(perso.x * echelle[0]), int(perso.y * echelle[1])))
+    frame_rect = pygame.Rect(frame * perso.w, 0, perso.h, perso.w)
+    fenetre.blit(perso_run_up, dest=( int(perso.x * echelle[0]), int(perso.y * echelle[1])), area=frame_rect)
+
     for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+
+        mvtx,mvty = 0,0
+
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             continuer = False
-
-        dt = clock.tick(60) # On ralentit la boucle à 60 FPS
-                
-        if event.type == KEYDOWN :
-
-            if event.key == K_UP:
-
-                if perso_y >= 490*leng:
-                    perso_y -= 8*leng                    
-                frame = (frame + 1) % 10
-                frame_rect = pygame.Rect(frame * WIDTH, 0, h, w)
-                fenetre.blit(arena, xdisp,ydisp)
-                fenetre.blit(layer, xdisp,ydisp)
-                fenetre.blit(perso_run_up, dest=(perso_x, perso_y), area=frame_rect)
-
-            if event.key == K_DOWN:
-
-                if perso_y <= 904*leng:
-                    perso_y += 8*leng
-                    
-                frame = (frame + 1) % 10
-                frame_rect = pygame.Rect(frame * WIDTH, 0, h, w)
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_run_up, dest=(perso_x, perso_y), area=frame_rect)
-
-            if event.key == K_LEFT:
-
-                if perso_x >= 513*leng:
-                    perso_x -= 8*leng
-                    
-                frame = (frame + 1) % 10
-                frame_rect = pygame.Rect(frame * WIDTH, 0, h, w)
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_run_up, dest=(perso_x, perso_y), area=frame_rect)
-
-            if event.key == K_RIGHT:
-
-                if perso_x <= 1350*leng:
-                    perso_x += 8*leng
-                    
-                frame = (frame + 1) % 10
-                frame_rect = pygame.Rect(frame * WIDTH, 0, h, w)
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_run_up, dest=(perso_x, perso_y), area=frame_rect)
-                
-        if event.type == KEYUP:
-
-            if event.key == K_UP:
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_up, (perso_x,perso_y))
-
-            if event.key == K_DOWN:
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_up, (perso_x,perso_y))
-
-            if event.key == K_LEFT:
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_up, (perso_x,perso_y))
-
-            if event.key == K_RIGHT:
-                fenetre.blit(arena, (xdisp,ydisp))
-                fenetre.blit(layer, (xdisp,ydisp))
-                fenetre.blit(perso_up, (perso_x,perso_y))
         
-        if 900*leng < perso_x < 1020*leng and 480*leng < perso_y < 550*leng and trigger == 0:
+        if event.type == pygame.KEYDOWN :
+
+            if event.key == pygame.K_UP:
+                mvty = -speed
+
+            if event.key == pygame.K_DOWN:
+                mvty = speed
+
+            if event.key == pygame.K_LEFT:
+                mvtx = -speed
+
+            if event.key == pygame.K_RIGHT:
+                mvtx = speed
+
+            frame = (frame + 1) % 10
+
+            mvt = Rect(perso.x+mvtx,perso.y+mvty,perso.w,perso.h)
+            print('mvt=',mvt)
+
+            if dedans(salle,mvt):
+                perso = mvt
+            elif mvty == -speed:
+                perso = Rect(perso.x,salle.y,perso.w,perso.h)
+            elif mvty == speed:
+                perso = Rect(perso.x,salle.y+salle.h-perso.h,perso.w,perso.h)
+            elif mvtx == -speed:
+                perso = Rect(salle.x,perso.y,perso.w,perso.h)
+            elif mvtx == speed:
+                perso = Rect(salle.x+salle.w-perso.w,perso.y,perso.w,perso.h)
+
+        if touche(pn,perso) and trigger == 0:
             trigger = 1
             pygame.mixer.music.load(f"{path}/Musique/Emerald Dream.wav")
             pygame.mixer.music.play()
             arena = pygame.image.load(f"{sprites_dir}/level1/Hooded_One_arena.png").convert_alpha()
+            arena = pygame.transform.scale(arena, (scrwidth,scrheight))
             layer = pygame.image.load(f"{sprites_dir}/level1/Hooded_One_layerblit.png").convert_alpha()
+            layer = pygame.transform.scale(layer, (scrwidth,scrheight))
             fond = pygame.image.load(f"{sprites_dir}/level1/Hooded_One_back.png").convert_alpha()
+            fond = pygame.transform.scale(fond, (scrwidth,scrheight))
             fenetre.blit(fond, (xdisp,ydisp))
-    
-    #niveau+=1
+
     pygame.display.flip()       
 
 pygame.quit()
